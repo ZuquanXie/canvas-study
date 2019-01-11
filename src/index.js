88422af9c3e41f053b12e484328b1c3fed2c481c
zuquanxie <designer_xzq@163.com>
2019-01-11 17:50:39 +0800

    temp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import './index.css';
 
-window.onload = function() {
-    const container = document.createElement('div');
-    container.setAttribute('id', 'mainContainer');
-    document.body.appendChild(container);
-    const a = { a: 1, b: 2 };
-    const b = { ...a };
-    console.log(b)
-};
+/* 像素操作 */
+import './imageData';
